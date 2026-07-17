9041e8c0b7ec89fb9145400eacf0a0e151711512
emvmart23 <max.martinez@tecsup.edu.pe>
2026-07-17 11:22:56 -0500

    feat(auth): implement sign-up page and form styles

--- src/pages/auth/sign-up/components/SignUpForm/index.tsx
+++ src/pages/auth/sign-up/components/SignUpForm/index.tsx
@@ -1,9 +1,11 @@
 import { Button, Input, InputPassword } from "../../../../../components/ui";
 import styles from "../../../Auth.module.css"
+import localStyles from "./SignUpForm.module.css"
 
 export default function SignUpForm() {
   return (
     <form className={styles.form}>
+      <div className={localStyles.formFielGroup}>
         <div className={styles.formField}>
           <label>Nombre</label>
           <Input id="firstName" type="text" placeholder="Tu nombre" />
@@ -12,23 +14,24 @@ export default function SignUpForm() {
           <label>Apellido</label>
           <Input id="lastName" type="text" placeholder="Tu apellido" />
         </div>
-        <div className={styles.formField}>
-          <label>Correo electrónico</label>
-          <Input id="email" type="email" placeholder="Tu correo electrónico" />
-        </div>
-        <div className={styles.formField}>
-          <label>Contraseña</label>
-          <InputPassword />
-        </div>
-        <div className={styles.formField}>
-          <label>Repetir contraseña</label>
-          <InputPassword
-            placeholder="Confirma tu contraseña"
-          />
-        </div> 
-        <Button className={styles.submitButton}>
-          Enviar
-        </Button>
-      </form>
+      </div>
+      <div className={styles.formField}>
+        <label>Correo electrónico</label>
+        <Input id="email" type="email" placeholder="Tu correo electrónico" />
+      </div>
+      <div className={styles.formField}>
+        <label>Contraseña</label>
+        <InputPassword />
+      </div>
+      <div className={styles.formField}>
+        <label>Repetir contraseña</label>
+        <InputPassword
+          placeholder="Confirma tu contraseña"
+        />
+      </div>
+      <Button className={styles.submitButton}>
+        Enviar
+      </Button>
+    </form>
   )
 }
--- src/pages/auth/sign-up/page/index.tsx
+++ src/pages/auth/sign-up/page/index.tsx
@@ -1,12 +1,19 @@
 import { SignUpForm } from "../components";
 import styles from "../../Auth.module.css";
+import { ArrowLeftIcon } from "lucide-react";
+import { Link } from "react-router";
 
 export default function SignUp() {
   return (
     <div className={styles.container}>
-      <h2>Crear cuenta</h2>
+      <div className={styles.header}>
+        <Link to="/auth/sign-in" className={styles.backLink} aria-label="Volver a iniciar sesión">
+          <ArrowLeftIcon size={20} />
+        </Link>
+        <h2>Crear cuenta</h2>
+      </div>
       <p>Regístrate para crear tu cuenta</p>
-      <SignUpForm/>
+      <SignUpForm />
     </div>
   );
 }
