743ce99c4292189b0b64052be0da3a5994bc7cba
emvmart23 <max.martinez@tecsup.edu.pe>
2026-07-17 11:23:26 -0500

    feat(auth): improve sign-in page implementation

--- src/pages/auth/sign-in/page/index.tsx
+++ src/pages/auth/sign-in/page/index.tsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router";
 import { Button, Input, InputPassword } from "../../../../components/ui";
 import styles from "../../Auth.module.css";
+import { ArrowLeftIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 export default function SignIn() {
+  const {register, handleSubmit  , formState:{ errors }} = useForm({
+    resolver:zodResolver({})
+  })
   return (
     <div className={styles.container}>
-      <h2>Iniciar sesion</h2>
+      <div className={styles.header}>
+        <Link to="/" className={styles.backLink} aria-label="Volver al inicio">
+          <ArrowLeftIcon size={20} />
+        </Link>
+        <h2>Iniciar sesion</h2>
+      </div>
       <p>Inicia sesión usando tu correo y contraseña</p>
-
       <form className={styles.form}>
         <div className={styles.formField}>
           <label htmlFor="email">Correo electrónico</label>
@@ -15,7 +25,7 @@ export default function SignIn() {
 
         <div className={styles.formField}>
           <label htmlFor="password">Contraseña</label>
-          <InputPassword placeholder="Tu contraseña"/>
+          <InputPassword placeholder="Tu contraseña" />
           <Link to="/auth/forgot-password" className={styles.links}>
             ¿Olvidaste tu contraseña?
           </Link>
